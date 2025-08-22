6b0c2be4d11415fe29571e3f1d9807b4fe819572
Team Azonix <azonixteam.contact@gmail.com>
2025-08-21 19:57:27 -0600

    Update e401cf5.js

--- _nuxt/e401cf5.js
+++ _nuxt/e401cf5.js
@@ -20614,7 +20614,7 @@
                                     }, {
                                         hid: "description",
                                         name: "description",
-                                        content: "Welcome to Team Abnormals! We are a small modding team focused on creating mods with quality art and design for Minecraft Java Edition."
+                                        content: "Welcome to Team Abnormals! We are a small modding team focused on creating mods with quality art and design for both Minecraft Editions."
                                     }, {
                                         name: "format-detection",
                                         content: "telephone=no"
@@ -20644,7 +20644,7 @@
                                         content: "https://www.teamabnormals.com"
                                     }, {
                                         property: "og:description",
-                                        content: "Welcome to Team Abnormals! We are a small modding team focused on creating mods with quality art and design for Minecraft Java Edition."
+                                        content: "Welcome to Team Abnormals! We are a small modding team focused on creating mods with quality art and design for both Minecraft Editions."
                                     }, {
                                         property: "og:image",
                                         content: "https://www.teamabnormals.com/img/banner.png"
@@ -20662,7 +20662,7 @@
                                         content: "https://www.teamabnormals.com"
                                     }, {
                                         property: "twitter:description",
-                                        content: "Welcome to Team Abnormals! We are a small modding team focused on creating mods with quality art and design for Minecraft Java Edition."
+                                        content: "Welcome to Team Abnormals! We are a small modding team focused on creating mods with quality art and design for both Minecraft Editions."
                                     }, {
                                         property: "twitter:image",
                                         content: "https://www.teamabnormals.com/img/banner.png"
